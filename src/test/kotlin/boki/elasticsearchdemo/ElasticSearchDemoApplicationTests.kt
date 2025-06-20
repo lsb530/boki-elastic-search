1780443f9725fae63b787c4c6113bdf18c7f2f2f
boki <lsb530@naver.com>
2025-06-20 22:23:55 +0900

    init project

---
+++ src/test/kotlin/boki/elasticsearchdemo/ElasticSearchDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package boki.elasticsearchdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElasticSearchDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
